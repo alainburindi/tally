807e55453d47252a88e17ca26d2abcab54c27b84
alainburindi <alainburindi62@gmail.com>
2020-05-30 13:53:00 +0200

    allow the user to view the poll and vote

--- controllers/vote.js
+++ controllers/vote.js
@@ -24,4 +24,23 @@ const createVote = async (req, res) => {
   return sendResponse(res, 201, "vote created", { vote: vote });
 };
 
-export { createVote };
+const viewVote = async (req, res) => {
+  const vote = await Vote.findOne({
+    where: { id: req.params.id },
+    include: [Choice],
+  });
+
+  if (!vote) return sendResponse(res, 404, "vote not found");
+
+  return sendResponse(res, 200, "voute found", { vote: vote });
+};
+
+const submitChoice = async (req, res) => {
+  const { id } = req.body;
+  const choice = await Choice.findByPk(id);
+  if (!choice) return sendResponse(res, 400, "Invalid choice");
+  choice.votes += 1;
+  await choice.save();
+  return sendResponse(res, 200, "choice submitted successfully");
+};
+export { createVote, viewVote, submitChoice };
--- routes/vote.js
+++ routes/vote.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import authenticate from "../middleware/authenticate";
-import { createVote } from "../controllers/vote";
+import { createVote, viewVote, submitChoice } from "../controllers/vote";
 
 const router = Router();
 
 router.post("/", authenticate, createVote);
+router.get("/single/:id", viewVote);
+router.post("/submit", submitChoice);
+
+router.get("/:id", (req, res) => res.render("vote"));
 
 export default router;
