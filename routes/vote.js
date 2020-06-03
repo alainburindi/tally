fab125296f0b21e37f135a15ac4c2ddb71d6f2a6
alainburindi <alainburindi62@gmail.com>
2020-06-03 15:05:43 +0200

    allow the user to delete a vote

--- routes/vote.js
+++ routes/vote.js
@@ -1,6 +1,12 @@
 import { Router } from "express";
 import authenticate from "../middleware/authenticate";
-import { createVote, viewVote, submitChoice } from "../controllers/vote";
+import {
+  createVote,
+  viewVote,
+  submitChoice,
+  deleteVote,
+  getUserVotes,
+} from "../controllers/vote";
 import optionalAuth from "../middleware/optionalauth";
 
 const router = Router();
@@ -11,5 +17,9 @@ router.get("/single/:id", optionalAuth, viewVote);
 router.post("/submit", submitChoice);
 
 router.get("/:id", (req, res) => res.render("vote"));
+
+router.delete("/", authenticate, deleteVote);
+
+router.get("/u/mine", authenticate, getUserVotes);
 
 export default router;
